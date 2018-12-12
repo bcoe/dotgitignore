549d8ac1fdb9537b4efb42dffc17fdd783279a54
ericksoen <michael.ericksen@gmail.com>
2018-12-11 23:58:52 -0600

    test: add assertions to test cases (#5)

--- test.js
+++ test.js
@@ -7,11 +7,11 @@ require('chai').should()
 describe('dotgitignore', () => {
   describe('ignore', () => {
     it('should return true for ignored files', () => {
-      dotgit.ignore('.DS_Store')
+      dotgit.ignore('.DS_Store').should.equal(true)
     })
 
     it('should return false for files that are not ignored', () => {
-      dotgit.ignore('README.md')
+      dotgit.ignore('README.md').should.equal(false)
     })
   })
 })
